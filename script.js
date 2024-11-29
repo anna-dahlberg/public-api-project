f0e62c5a91b290b9613090e8bcf847a19a48c0e2
anna-dahlberg <anna.dahlberg@live.se>
2024-11-29 14:53:32 +0100

    added field for emptying the text container before displaying quote

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const url = 'https://api.adviceslip.com/advice'
 
 const button = document.querySelector(".advice-button");
 
-const getAdvice = async() => {
+const getAdvice = async () => {
     const response = await fetch(url);
     const advice = await response.json();
     
+    const adviceContainer = document.querySelector(".advice");
+    
+    adviceContainer.innerHTML = '';  //Clears the previous response
+    
     const printAdvice = document.createElement("p");
     printAdvice.innerText = advice.slip.advice;
-
-    const adviceContainer = document.querySelector(".advice"); 
     adviceContainer.appendChild(printAdvice);
 };
 
 
 button.addEventListener("click", function() {
     getAdvice();
-})
+});
+
+
 
 
 
